fcdfca5639da6109af23b1fa988ea0b98167c7df
aaklabs <connectakonline@gmail.com>
2025-05-03 22:57:55 -0400

    Setup
    Working Project Setup with dependencies

---
+++ src/main/java/com/connectaak/ai/SqlAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.connectaak.ai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlAiAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlAiAgentApplication.class, args);
+	}
+
+}
